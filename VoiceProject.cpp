a6014949734f5aab022e3348dea93024a91a2e49
gamingzeether <80543808+gamingzeether@users.noreply.github.com>
2024-07-10 21:49:20 -0700

    fft

--- VoiceProject.cpp
+++ VoiceProject.cpp
@@ -1,8 +1,12 @@
+#define _DISABLE_VECTOR_ANNOTATION
+#define _DISABLE_STRING_ANNOTATION
+
 #include <iostream>
 #include <algorithm>
 #include <cstdlib>
 #include <rtaudio/RtAudio.h>
-#include <rtaudio/rtaudio_c.h>
+#include <pffft/pffft.h>
+
 
 typedef float INPUT_TYPE;
 #define INPUT_FORMAT RTAUDIO_FLOAT32
@@ -12,9 +16,11 @@ typedef float OUTPUT_TYPE;
 #define OUTPUT_FORMAT RTAUDIO_FLOAT32
 #define OUTPUT_SCALE  1.0
 
-#define INPUT_BUFFER_TIME 5
-#define INPUT_BUFFER_SIZE 2048
-#define OUTPUT_BUFFER_SIZE 2048
+#define INPUT_BUFFER_TIME 1
+#define INPUT_BUFFER_SIZE 128
+#define OUTPUT_BUFFER_SIZE 128
+
+#define FFT_FRAME_SAMPLES 128
 
 struct InputData {
     INPUT_TYPE* buffer;
@@ -27,6 +33,7 @@ struct InputData {
 struct OutputData {
     double* lastValues;
     unsigned int channels;
+    unsigned long lastSample;
     InputData* input;
 };
 
@@ -36,7 +43,7 @@ int processInput(void* /*outputBuffer*/, void* inputBuffer, unsigned int nBuffer
     InputData* iData = (InputData*)data;
 
     for (int i = 0; i < nBufferFrames; i++) {
-        unsigned int writePosition = (iData->writeOffset + i) % iData->bufferBytes;
+        unsigned int writePosition = (iData->writeOffset + i) % iData->totalFrames;
         iData->buffer[writePosition] = ((INPUT_TYPE*)inputBuffer)[i];
     }
     iData->writeOffset += nBufferFrames;
@@ -55,14 +62,16 @@ int processOutput(void* outputBuffer, void* /*inputBuffer*/, unsigned int nBuffe
     }
 
     InputData* iData = oData->input;
+    unsigned int startSample = oData->lastSample;
     for (int i = 0; i < oData->channels; i++) {
         for (int j = 0; j < nBufferFrames; j++) {
-            unsigned int readPosition = (iData->writeOffset - nBufferFrames + j) % iData->bufferBytes;
+            unsigned int readPosition = (startSample + j) % iData->totalFrames;
             INPUT_TYPE input = iData->buffer[readPosition];
             double value = (double)input / INPUT_SCALE;
             *buffer++ = (OUTPUT_TYPE)(value * OUTPUT_SCALE * 0.5);
         }
     }
+    oData->lastSample = startSample + nBufferFrames;
 
     return 0;
 }
@@ -89,18 +98,23 @@ int main() {
     }
     RtAudio::DeviceInfo inputInfo = inputAudio.getDeviceInfo(defaultInput);
 
+    std::cout << "Using input: " << inputInfo.name << '\n';
+    std::cout << "Sample rate: " << inputInfo.preferredSampleRate << '\n';
+
     inputParameters.deviceId = defaultInput;
-    inputParameters.nChannels = inputInfo.inputChannels;
+    inputParameters.nChannels = 1; // inputInfo.inputChannels;
     unsigned int sampleRate = inputInfo.preferredSampleRate;
     unsigned int bufferFrames = INPUT_BUFFER_SIZE;
 
     InputData inputData;
-    inputData.bufferBytes = bufferFrames * inputParameters.nChannels * sizeof(INPUT_TYPE);
     inputData.totalFrames = (unsigned long)(sampleRate * INPUT_BUFFER_TIME);
     inputData.channels = inputParameters.nChannels;
     unsigned long totalBytes;
     totalBytes = inputData.totalFrames * inputParameters.nChannels * sizeof(INPUT_TYPE);
-    inputData.buffer = (INPUT_TYPE*)malloc(totalBytes);
+    inputData.buffer = new INPUT_TYPE[totalBytes];
+    for (int i = 0; i < inputData.totalFrames; i++) {
+        inputData.buffer[i] = 0;
+    }
     inputData.writeOffset = 0;
 
     if (inputAudio.openStream(NULL, &inputParameters, INPUT_FORMAT,
@@ -131,6 +145,9 @@ int main() {
     }
     RtAudio::DeviceInfo outputInfo = outputAudio.getDeviceInfo(defaultOutput);
 
+    std::cout << "Using output: " << outputInfo.name << '\n';
+    std::cout << "Sample rate: " << outputInfo.preferredSampleRate << '\n';
+
     outputParameters.deviceId = defaultOutput;
     outputParameters.nChannels = outputInfo.outputChannels;
     unsigned int outputSampleRate = outputInfo.preferredSampleRate;
@@ -155,21 +172,39 @@ int main() {
     }
 #pragma endregion
 
-    // Test input
-    DWORD sleepTime = (DWORD)1000 * INPUT_BUFFER_TIME;
-    unsigned int counter = 0;
+    PFFFT_Setup* pffftSetup = pffft_new_setup(FFT_FRAME_SAMPLES, pffft_transform_t::PFFFT_REAL);
+    float* fft_in = (float*)pffft_aligned_malloc(FFT_FRAME_SAMPLES * sizeof(float));
+    float* fft_out = (float*)pffft_aligned_malloc(FFT_FRAME_SAMPLES * sizeof(float));
+    float* fft_work = (float*)pffft_aligned_malloc(FFT_FRAME_SAMPLES * sizeof(float));
+
+    std::vector<double> x = std::vector<double>(FFT_FRAME_SAMPLES / 2);
+    std::vector<double> samples = std::vector<double>(FFT_FRAME_SAMPLES / 2);
+
+    int counter = 0;
     while (inputAudio.isStreamRunning()) {
-        Sleep(sleepTime);
-        std::cout << counter << "\n";
-        counter++;
-        //for (int i = 0; i < totalBytes; i += 100) {
-        //    unsigned int readPos = (userData.writeOffset + i) % userData.bufferBytes;
-        //    //std::cout << userData.buffer[readPos] << '\n';
-        //}
+        Sleep(1000);
+
+        unsigned int start = inputData.writeOffset - INPUT_BUFFER_SIZE;
+        for (int i = 0; i < FFT_FRAME_SAMPLES; i++) {
+            unsigned int readLocation = (start + i) % inputData.totalFrames;
+            float data = inputData.buffer[readLocation];
+            fft_in[i] = data;
+            //x[i] = i;
+            //samples[i] = data;
+        }
+
+        pffft_transform_ordered(pffftSetup, fft_in, fft_out, fft_work, PFFFT_FORWARD);
+
+        for (int i = 0; i < FFT_FRAME_SAMPLES / 2; i++) {
+            x[i] = (i * sampleRate) / (FFT_FRAME_SAMPLES * 2);
+            samples[i] = abs(fft_out[i]);
+        }
     }
 
-    cleanupRtAudio(inputAudio);
-    cleanupRtAudio(outputAudio);
+    pffft_destroy_setup(pffftSetup);
+    pffft_aligned_free(fft_in);
+    pffft_aligned_free(fft_out);
+    pffft_aligned_free(fft_work);
 
     return 0;
 }
