3311f2ef87740f995a7887e515bbf84217fff2e7
gamingzeether <80543808+gamingzeether@users.noreply.github.com>
2025-04-12 20:16:23 -0700

    remove vestigial shaders

--- shaders/compileshaders.bat
+++
@@ -1,4 +0,0 @@
-C:/VulkanSDK/1.3.224.1/Bin/glslangValidator.exe -V shader_horizontal.vert -o vert_h.spv
-C:/VulkanSDK/1.3.224.1/Bin/glslangValidator.exe -V shader_vertical.vert -o vert_v.spv
-C:/VulkanSDK/1.3.224.1/Bin/glslangValidator.exe -V shader.frag -o frag.spv
-pause
--- shaders/compileshaders.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-glslangValidator -V shader_horizontal.vert -o vert_h.spv
-glslangValidator -V shader_vertical.vert -o vert_v.spv
-glslangValidator -V shader.frag -o frag.spv
